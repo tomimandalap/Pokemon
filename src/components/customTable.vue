e83e2802ffcf3005db29d0ccbb06651769bc9c8d
tomimandalap <tomi.mandala@privyid.tech>
2022-10-08 19:34:25 +0700

    feat: add component customTable

---
+++ src/components/customTable.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  items: {
+    type: Array,
+    default: () => {
+      [];
+    },
+  },
+  title: { type: String, default: "" },
+  variant: { type: String, default: "" },
+  dark: { type: Boolean, default: false },
+});
+</script>
+<template>
+  <h5 class="text-dark">{{ title }}</h5>
+  <b-table
+    hover
+    bordered
+    sticky-header
+    responsive="sm"
+    head-variant="light"
+    :dark="dark"
+    :table-variant="variant"
+    :items="items"
+  ></b-table>
+</template>
