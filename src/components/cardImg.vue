21282c7e3637bfd55be2b4869d904fb42435baab
tomimandalap <tomi.mandala@privyid.tech>
2022-10-08 19:04:49 +0700

    feat: add component cardimg

---
+++ src/components/cardImg.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  url: null,
+});
+</script>
+<template>
+  <b-col v-if="url" md="6" sm="6" cols="12" class="my-2">
+    <b-card
+      data-aos="zoom-in-up"
+      :img-src="url"
+      img-alt="Image"
+      img-top
+      tag="article"
+      class="w-100 h-100"
+    ></b-card>
+  </b-col>
+</template>
