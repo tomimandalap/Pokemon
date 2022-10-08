e382310b259064b8348211da1f5727b055070246
tomimandalap <tomi.mandala@privyid.tech>
2022-10-08 19:46:55 +0700

    chore: add coloum size lg at component cardImg

--- src/components/cardImg.vue
+++ src/components/cardImg.vue
@@ -4,7 +4,7 @@ defineProps({
 });
 </script>
 <template>
-  <b-col v-if="url" md="6" sm="6" cols="12" class="my-2">
+  <b-col v-if="url" lg="6" md="12" sm="6" cols="12" class="my-2">
     <b-card
       data-aos="zoom-in-up"
       :img-src="url"
